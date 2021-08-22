b419854526298310a66033af098debc4e839a993
ADarthGalacticEmpire <amanda.lee.crotty@gmail.com>
2021-08-22 15:56:09 -0400

    added outline to program

---
+++ SampleProject/DatabaseApp/CrottySQLConnector/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrottySQLConnector
+{
+    static class Program //extends Application
+    {
+        //private Connetion connect = null;
+
+        //create objects for retrieval by user
+        //private CheckBox cbCustid = new CheckBox();
+
+        //where clause for search
+        //private TextField tfWhere = new TextField();
+
+        //output
+        //private TextArea taResult = new TextArea();
+
+        //search and clear buttons
+        //private Button btSearch = new Button("Search");
+        //private Button btClear = new Button("Clear");
+
+        //Window for UI
+        //GridPane dbSearch = new GridPane();
+
+
+        //BEGIN NEW
+        //Override for C# / Forms equivalent to Stage class.
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //launch args
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
